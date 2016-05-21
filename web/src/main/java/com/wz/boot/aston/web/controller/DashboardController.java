aa7f229de34a8167b9f968a70de109ec54808ee9
wenzeng <wenzeng@mogujie.com>
2016-05-22 01:09:52 +0800

    commit

--- web/src/main/java/com/wz/boot/aston/web/controller/DashboardController.java
+++ web/src/main/java/com/wz/boot/aston/web/controller/DashboardController.java
@@ -1,11 +1,8 @@
 package com.wz.boot.aston.web.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.wz.boot.aston.domain.entity.BaseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author wenzeng
@@ -14,32 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DashboardController {
 
-    @RequestMapping({"/dashboard", "/index", "/main", "/"})
+    @RequestMapping({"/dashboard", "/main", "/"})
     public String dashboard() {
         return "dashboard";
     }
 
-    @RequestMapping({"/icon"})
-    public String icon() {
-        return "icon";
-    }
+    @RequestMapping("dict")
+    public void dict(ModelMap modelMap) {
 
-    @RequestMapping({"success"})
-    @ResponseBody
-    public Object json() {
-        return "success";
-    }
-
-    @RequestMapping("form")
-    public void form(ModelMap modelMap) {
-
-    }
-
-    @RequestMapping("save")
-    @ResponseBody
-    public Object save(BaseEntity entity) {
-        System.out.println("BaseEntity:"+JSON.toJSONString(entity));
-        return "OK";
     }
 }
 
